186bb05a734a423155decee8891ac1063a9d1be2
abrwin <tidusxt@hotmail.com>
2017-05-02 13:51:58 -0500

    Se cambia logo de app y se afinan detalles de reproduccion

---
+++ app/src/main/java/com/lapantallasoftware/collectivecrew/ccapp/utils/MyMediaPlayer.java
@@ -0,0 +1,64 @@
+package com.lapantallasoftware.collectivecrew.ccapp.utils;
+
+import android.widget.MediaController;
+
+/**
+ * Created by AbrWin on 29/04/17.
+ */
+
+public class MyMediaPlayer implements MediaController.MediaPlayerControl {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int i) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+}
